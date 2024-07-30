e9fb5c001f87c4e282243f112a8d341c0c8e5933
lausan3 <lauanthony4455@gmail.com>
2024-07-30 15:34:48 -0400

    Add: Supabase type safety via type definitions

---
+++ src/client/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./supabase";
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_API_KEY;
+
+export const KEY_SPOTIFY_TABLE = "spotify-auth";
+
+export const supabase = createClient<Database>(SUPABASE_URL || "", SUPABASE_KEY || "");
