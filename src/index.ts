d3f7944ccbabf002104e055f9ae68463ef1e764a
lausan3 <lauanthony4455@gmail.com>
2024-07-30 01:49:41 -0400

    Refactor: Group Spotify endpoints

--- src/index.ts
+++ src/index.ts
@@ -15,97 +15,101 @@ let accessToken: string | null = null;
 
 const app = new Elysia()
   .get("/", () => "Hello from Elysia!")
-  .all('/spotify/authorize', async () => {
-    if (accessToken) {
-      return await fetch(`${baseUri}/spotify/lastplayed`);
-    }
-
-    const scope = "user-read-recently-played";
-
-    const baseUrl = "https://accounts.spotify.com/authorize";
-
-    return redirect(`${baseUrl}?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${baseUri}/spotify/callback`)
-  })
-  .get('/spotify/callback', async ({ query }) => {
-    const code = query.code || null;
-    const error = query.error || null;
-
-    if (!code || error) {
-      return error;
-    }
-
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: `${baseUri}/spotify/callback`,
-        grant_type: 'authorization_code',
-      },
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
-      },
-      json: true,
-    };
-
-    try {
-      const response = await fetch(authOptions.url, {
-        method: 'POST',
-        headers: authOptions.headers,
-        body: new URLSearchParams(authOptions.form),
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        accessToken = data.access_token;
-
-        return await fetch(`${baseUri}/spotify/lastplayed`);
-      } else {
-        return 'Failed to authenticate';
-      }
-    } catch (error) {
-      return error;
-    }
-  }, {
-    query: t.Object({
-      code: t.MaybeEmpty(t.String()),
-      error: t.MaybeEmpty(t.String())
-    })
-  })
-  .get('/spotify/lastplayed', async () => {
-    if (!accessToken) return "No access token, try authorizing.";
-
-    const url = "https://api.spotify.com/v1/me/player/recently-played?limit=1";
-
-    try {
-      const response = await fetch(url,
-        {
+  .group('/spotify', app => {
+    return app
+      .all('/authorize', async () => {
+        if (accessToken) {
+          return await fetch(`${baseUri}/spotify/lastplayed`);
+        }
+        
+        const scope = "user-read-recently-played";
+        
+        const baseUrl = "https://accounts.spotify.com/authorize";
+        
+        return redirect(`${baseUrl}?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${baseUri}/spotify/callback`)
+      })
+      .get('/callback', async ({ query }) => {
+        const code = query.code || null;
+        const error = query.error || null;
+        
+        if (!code || error) {
+          return error;
+        }
+        
+        const authOptions = {
+          url: 'https://accounts.spotify.com/api/token',
+          form: {
+            code: code,
+            redirect_uri: `${baseUri}/spotify/callback`,
+            grant_type: 'authorization_code',
+          },
           headers: {
-            "authorization": `Bearer ${accessToken}`
+            'content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+          },
+          json: true,
+        };
+        
+        try {
+          const response = await fetch(authOptions.url, {
+            method: 'POST',
+            headers: authOptions.headers,
+            body: new URLSearchParams(authOptions.form),
+          });
+          
+          if (response.ok) {
+            const data = await response.json();
+            accessToken = data.access_token;
+            
+            return await fetch(`${baseUri}/spotify/lastplayed`);
+          } else {
+            return 'Failed to authenticate';
           }
+        } catch (error) {
+          return error;
         }
-      );
-      const json = await response.json();
-      
-      if (response.ok) {
-
-        const lastSong = json.items[0].track;
-
-        const ret: ShieldsJSONFormat = {
-          schemaVersion: 1,
-          label: "Last Song",
-          message: `${lastSong.name} by ${lastSong.artists[0].name}`
+      }, {
+        query: t.Object({
+          code: t.MaybeEmpty(t.String()),
+          error: t.MaybeEmpty(t.String())
+        })
+      })
+      .get('/lastplayed', async () => {
+        if (!accessToken) return "No access token, try authorizing.";
+        
+        const url = "https://api.spotify.com/v1/me/player/recently-played?limit=1";
+        
+        try {
+          const response = await fetch(url,
+            {
+            headers: {
+              "authorization": `Bearer ${accessToken}`
+            }
+          }
+        );
+        const json = await response.json();
+        
+        if (response.ok) {
+          
+          const lastSong = json.items[0].track;
+          
+          const ret: ShieldsJSONFormat = {
+            schemaVersion: 1,
+            label: "Last Song",
+            message: `${lastSong.name} by ${lastSong.artists[0].name}`,
+            namedLogo: "spotify"
+          }
+          
+          return ret;
+        } else {
+          return json.error;
         }
-
-        return ret;
-      } else {
-        return json.error;
+      } catch (error) {
+        return error;
       }
-    } catch (error) {
-      return error;
-    }
-  })
-  .listen(3000);
+    })
+  }) // End /Spotify group
+.listen(3000);
 
 const redirect_uri = `${app.server?.hostname}`
 
