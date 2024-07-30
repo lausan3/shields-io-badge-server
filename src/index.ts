20bc39afb75ee3a0dc0882ea25236c9539025aba
lausan3 <lauanthony4455@gmail.com>
2024-07-29 22:18:39 -0400

    Refactor: Test Lastplayed endpoint to be inside index.ts to test api hitting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Elysia } from "elysia";
 
+// Endpoints
+import ShieldsJSONFormat from "./shields/shields-format";
+
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
+  .all('/spotify/lastplayed/:id', ({ params: { id }}) => {
+    const ret: ShieldsJSONFormat = {
+      schemaVersion: "1",
+      label: "Last Played Song",
+      message: id,
+    }
+
+    return ret;
+  })
   .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+// Test last played feature
+app.handle(new Request('http://localhost:3000/spotify/lastplayed/5')).then(console.log);
